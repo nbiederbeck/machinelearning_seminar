bff16479fa7ed5f71b4b784836603acb7f60efa6
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-06-25 19:29:49 +0200

    add test script for Rainbow* to describe usage

---
+++ test/test_rainbow.py
@@ -0,0 +1,23 @@
+from RainbowPainter import RainbowPainter
+from RainbowCutter import RainbowCutter
+from RainbowPlotter import RainbowPlotter
+
+
+def main():
+    pi = 3.1415962
+
+    painter = RainbowPainter()
+    cutter = RainbowCutter()
+    plotter = RainbowPlotter()
+
+    r, g, b = 0, 1, 2
+
+    mesh = painter.paint_mesh(r, g, b, 156)
+    cut = cutter.cut_mesh(mesh, r, g, b, pi / 4)
+
+    fig, ax = plotter.plot_colormesh(cut)
+    fig.savefig("build/test_rainbow.png", dpi=256 / 4)
+
+
+if __name__ == "__main__":
+    main()
