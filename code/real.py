21a077595600c1bef532e2edf1b66d839f175a17
Maximilian Sackel <maximilian.sackel@gmx.de>
2018-07-10 13:25:46 +0200

    add verschiebung

--- code/real.py
+++ code/real.py
@@ -1,29 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def i_f(x, y):
-    resp = []
-    for x_i, y_i in zip(x, y):
-        resp.append( 4*x_i**2 > y_i )
-    return resp
-
 def rotate(x,y, theta):
     xs = np.cos(theta)*x + np.sin(theta)*y
     ys = -np.sin(theta)*x + np.cos(theta)*y
     return xs ,ys
 
+def squared(x, x_0, y_0, a):
+    return a*(x-x_0)**2 + y_0
+
+def rotate_func(x, y, theta):
+    xs, ys = rotate(x, y, theta)
+    x_0 = 0.1
+    y_0 = 0.5
+    x_0s, y_0s = rotate(x_0, y_0, theta)
+    resp = []
+    for x_i, y_i in zip(xs, ys):
+        resp.append( squared(x_i, x_0s, y_0s, 4) > y_i )
+    return np.array(resp)
+
 x, y = np.meshgrid(np.linspace(0,1,256), np.linspace(0,1,256))
 x = x.flatten()
 y = y.flatten()
 
+mask = rotate_func(x,y, -np.pi/4)
 
-xs, ys = rotate(x, y, -np.pi/3)
-mask = i_f(xs,ys)
-mask = np.array(mask)
-
-plt.scatter(x, y, marker='.', label='unrotierter Raum1')
-plt.scatter(x[mask], y[mask], marker='.', label='unrotiertet Raum3')
-plt.xlim(0,1)
-plt.legend()
+plt.scatter(x, y, marker='.')
+plt.scatter(x[mask], y[mask], marker='.')
 
 plt.show()
