ddb90645617666255250b2a5272e07e1c9ec0f30
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-07-10 11:23:38 +0200

    add setup.py

---
+++ code/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mls",  # machine learning seminar
+    version="0.0.1",
+    description="Code for Machinelearning Seminar for Physicists",
+    url="https://github.com/nonator/machinelearning_seminar",
+    author="Biederbeck, Noah and Sackel, MAximilian",
+)
