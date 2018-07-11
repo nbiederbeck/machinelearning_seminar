fe50005784ee752d3dcb1a1064894daae4eb391b
Maximilian Sackel <maximilian.sackel@gmx.de>
2018-07-11 13:13:26 +0200

    früher war alles besser

--- code/mls/rainbow/cutimage.py
+++ code/mls/rainbow/cutimage.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class RainbowCutter:
-    def __init__(self, N):
+    def __init__(self, N, theta, scale, xshift=1, yshift=1, xbias=0, ybias=0):
         ''' initalize a 3dim Mesgrid with N points per axis 
         for calculation of the cuts
         Parameters:
@@ -11,8 +11,14 @@ class RainbowCutter:
             discretisation of axis
         '''
         self.r, self.b, self.g = np.meshgrid(
-            np.linspace(0, 1, N), np.linspace(0, 1, N), np.linspace(0, 1, N)
-        )
+                np.linspace(0, 1, N), np.linspace(0, 1, N), np.linspace(0, 1, N)
+                )
+        self.theta = theta
+        self.scale = scale
+        self.xshift = xshift
+        self.yshift = yshift
+        self.xbias = xbias
+        self.ybias = ybias
 
     def _rotate(self, x, y, theta):
         ''' Rotate given tuple of axis with theta at 
@@ -27,7 +33,7 @@ class RainbowCutter:
         yr = -np.sin(theta) * x + np.cos(theta) * y
         return xr, yr
 
-    def cut_function(self, x, y, x0, y0, theta, scale):
+    def cut_function(self, x, y, z):
         ''' cut function to seperate background from signal
         Parameters:
             x, y: ndarray(N,N,N)
@@ -43,27 +49,37 @@ class RainbowCutter:
                 mask of true and false for given 
                 points
         '''
-        x_rot, y_rot = self._rotate(x, y, theta)
-        x0_rot, y0_rot = self._rotate(x0, y0, theta)
+        x0 = z * self.xshift + self.xbias
+        y0 = z * self.yshift + self.ybias
+        x_rot, y_rot = self._rotate(x, y, self.theta)
+        x0_rot, y0_rot = self._rotate(x0, y0, self.theta)
 
         x = x_rot - x0_rot
         y = y_rot - y0_rot
 
-        mask = y < scale * (x * x)
+        mask = y < self.scale * (x * x)
 
         return mask
 
+    def cut_image(self, im):
+        mask = self.cut_function(*im)
+        im[~mask] = 0
+        return im
+
+
     def mask_cube(self):
         return self.cut_function(
-            self.r, self.g, 1.1 * self.b, 1.1 * self.b, -1 * np.pi / 3.5, -30
-        )
+                self.r,
+                self.g,
+                self.b
+            )
 
 
 class plot_cube:
     def __init__(self, mask, apply_mask=True):
         self.r, self.b, self.g = np.meshgrid(
-            np.linspace(0, 1, 256), np.linspace(0, 1, 256), np.linspace(0, 1, 256)
-        )
+                np.linspace(0, 1, 256), np.linspace(0, 1, 256), np.linspace(0, 1, 256)
+                )
         if apply_mask:
             self.r[~mask] = 0
             self.b[~mask] = 0
@@ -80,25 +96,31 @@ class plot_cube:
         rgb = np.array([r, g, b]).T
 
         color_tuple = rgb.transpose((1, 0, 2)).reshape(
-            (rgb.shape[0] * rgb.shape[1], rgb.shape[2])
-        )
+                (rgb.shape[0] * rgb.shape[1], rgb.shape[2])
+                )
 
         m = plt.pcolormesh(r, color=color_tuple, linewidth=0)
         m.set_array(None)
-        return r, color_tuple
+
+    def ful_plane(self):
+        for x in range(51):
+            print(x*5)
+            fig = plt.figure(figsize=(5,15))
+            for r in range(3):
+                plt.subplot('31{}'.format(r))
+                self.plot_plane(r, x*5)
+            plt.savefig('build/{}.png'.format(x*5))
+            plt.close()
 
 
 def main():
     N = 256
 
-    cutter = RainbowCutter(N)
+    cutter = RainbowCutter(N, -np.pi/3, -20,xshift=2.0, yshift=2.0, xbias=0.2, ybias=-0.3)
     mask_cube = cutter.mask_cube()
 
     pltter = plot_cube(mask_cube, True)
-
-    pltter.plot_plane(0, 150)
-    pltter.plot_plane(1, 150)
-    pltter.plot_plane(2, 150)
+    pltter.ful_plane()
 
 
 if __name__ == "__main__":
