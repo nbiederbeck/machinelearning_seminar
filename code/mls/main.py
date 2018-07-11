2defd5e7095d3ffc04ff5090f17dc813d91d0730
Noah Biederbeck <noah.biederbeck@udo.edu>
2018-07-11 15:26:55 +0200

    add main script for entire analysis, good idea??

---
+++ code/mls/main.py
@@ -0,0 +1,13 @@
+from mls import rainbow
+from mls import preprocessing
+
+
+def main():
+    """Main function to call for entire analysis,
+    including training, testing, making plots.
+    """
+    print(help(main))
+
+
+if __name__ == "__main__":
+    main()
